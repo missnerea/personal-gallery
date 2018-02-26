ce4077d1c46b7b5509cd3a0d18a4f93aedd727cd
missnerea <ela.amish@gmail.com>
2018-02-26 11:04:48 +0300

    Add hero-image and text

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'gallery.apps.GalleryConfig'
+    'gallery',
+    'bootstrap4'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
