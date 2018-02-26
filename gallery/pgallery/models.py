0a96e2581b748257b8ddc9a06d020d0452856a87
missnerea <ela.amish@gmail.com>
2018-02-26 22:35:54 +0300

    make migrations for image model

--- gallery/pgallery/models.py
+++ gallery/pgallery/models.py
@@ -5,18 +5,17 @@ from django.db import models
 class Category(models.Model):
     name=models.CharField(max_length=60)
 
-    def __str__:
+    def __str__(self):
         return self.name
 
-class Image(model.Models):
+class Image(models.Model):
     image=models.ImageField(upload_to='gallery/', blank=True)
     image_link=models.TextField(blank=True)
     image_name=models.TextField(max_length=60 , blank=True)
     details=models.TextField(max_length=60, blank=60)
     pub_date = models.DateTimeField(auto_now_add=True)
-    category=models.ManytoManyField(category)
-    location = models.ManytoManyField(location)
+    category = models.ManyToManyField(Category, blank=True)
 
-    def __str__:
+    def __str__(self):
         return self.image
 
