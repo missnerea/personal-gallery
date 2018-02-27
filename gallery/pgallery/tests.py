0659f205d1dac7d749e4255398dd109af0aa1727
missnerea <ela.amish@gmail.com>
2018-02-27 09:59:39 +0300

    add test for Image module

--- gallery/pgallery/tests.py
+++ gallery/pgallery/tests.py
@@ -2,3 +2,10 @@ from django.test import TestCase
 from .models import Category,Location,Image
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.image=Image(image_name='twins' details='cute twins')
+
+        def test_instance(self):
+            self.assertTrue(isinstance(self.image,Image))
