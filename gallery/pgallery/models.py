fc1da862a13aa7cfde4ede121cd10fa727ad3006
missnerea <ela.amish@gmail.com>
2018-02-27 09:23:21 +0300

    make location model migration to create location table

--- gallery/pgallery/models.py
+++ gallery/pgallery/models.py
@@ -20,7 +20,7 @@ class Image(models.Model):
         return self.image
 
 class Location(models.Model):
-    location=model.CharField(max_length=60)
+    location=models.CharField(max_length=60)
 
     def __str__(self):
         return self.location 
