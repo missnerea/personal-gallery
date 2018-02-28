9391bcf9c995f6449c378b412b3c3087619367fd
missnerea <ela.amish@gmail.com>
2018-02-28 09:30:12 +0300

    Pictures working

--- gallery/pgallery/urls.py
+++ gallery/pgallery/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
@@ -7,3 +8,5 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results')
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- gallery/pgallery/views.py
+++ gallery/pgallery/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-# from .models import image
+from .models import Image
 import datetime as dt
 # Create your views here.
 def index (request):
      title = 'Welcome to the gallery'
      date = dt.date.today()
-     
-     return render(request,'index.html')
+     pgallery = Image.objects.all()
+
+     return render(request,'index.html',{"pgallery":pgallery})
 
 
 def search_results(request):
